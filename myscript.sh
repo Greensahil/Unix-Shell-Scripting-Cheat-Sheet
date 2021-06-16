e5cd491b351c2a1a300f7f50e3f610dbd53323d3
unknown <ssharma2@svsu.edu>
2021-06-15 21:08:30 -0400

    user input more methids

--- myscript.sh
+++ myscript.sh
@@ -6,6 +6,10 @@
 
 #! /usr/bin/bash
 
+
+
+
+
 #ECHO COMMAND -- Works like a print command
 echo Hello World! 
 
@@ -14,6 +18,20 @@ echo Hello World!
 #Letters, numbers and underscores
 NAME="Sahil"
 echo "My name is ${NAME}"
+
+
+
+#Another way to read user input taught in college
+echo Enter a filename to examine:
+read FNAME
+echo The number of lines in $FNAME is:
+wc -l $FNAME
+echo THe number of words in $FNAME is:
+wc -w $FNAME
+echo End of processing for $FNAME
+
+
+
 
 
 # USER INPUT
@@ -50,3 +68,32 @@ fi
 # val1 -lt val2 Returns true if val1 is less than val2
 # val1 -le val2 Returns true if val1 is less than or equal to val2
 ########
+
+
+NUM1=3
+NUM2=5
+
+if [ "${NUM1}" -gt "${NUM2}" ]
+then
+    echo "${NUM1} is greater than ${NUM2}"
+else
+    echo "${NUM1} is less than ${NUM2}"
+fi
+
+
+
+
+
+
+
+
+
+
+#HOW STUFF EXECURES -- These commands are interpreted and not compiled
+
+# 1) The script is launched from within the user's login shell by typing the name of the shell script on the command line ./myscript
+# 2) The user's login shell reads the first line of the script.
+# 3) The shell which is named on the first line is executed and the name of the script is passed on to the new shell.
+# 4) The new shell executes each line of the script file in an interpreted fashion.
+# 5) The new shell exits at the end of the script.
+# 6) Control is then returned to the user's original login shell which is still running.
